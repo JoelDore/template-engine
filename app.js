7790ed6857750bcb5badc643f111fbfdc224a4a5
JES20 <dore.joel.dore@gmail.com>
2020-11-23 22:08:17 -0500

    Require at least one Manager per team

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 let team = [];
+let checkManager = false;
+
 function getInfo() {
     inquirer.prompt([
         {
@@ -61,12 +60,20 @@ function getInfo() {
             message: "Add another employee?"
         }
     ]).then((answers) => {
-        // Add employee to team
-        team.push(answers)
-        if (answers.newEmployee) {
-            getInfo()
-        }
-        else {
+
+        if (answers.role === "Manager") checkManager = true;
+
+        if (answers.newEmployee) { // Add another employee
+            delete answers.newEmployee;
+            team.push(answers);
+            getInfo();
+        } else if (!checkManager) { // Require a Manager
+            console.log("ERROR: Team must include a manager! Please add one now.");
+            delete answers.newEmployee;
+            team.push(answers);
+            getInfo();
+        } else {
+            console.log(team);
             // After the user has input all employees desired, call the `render` function (required
             // above) and pass in an array containing all employee objects; the `render` function will
             // generate and return a block of HTML including templated divs for each employee!
@@ -76,16 +83,6 @@ function getInfo() {
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
+// `output` folder. You can use the variable `outputPath` above to target this location.
+// HINT: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
